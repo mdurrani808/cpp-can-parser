2f060980609192f8eb044f9b2f92d7b74f097205
Mohammad Durrani <mdurrani808@gmail.com>
2026-04-19 19:05:18 -0400

    Add NS_DESC_ to NS_TOKENS set

--- src/parsing/DBCParser.cpp
+++ src/parsing/DBCParser.cpp
@@ -33,10 +33,11 @@ static std::set<std::string> SUPPORTED_DBC_TOKENS = {
 };
 
 static std::set<std::string> NS_TOKENS = {
+  "NS_DESC_",
   "CM_", "BA_DEF_", "BA_", "VAL_", "CAT_DEF_", "CAT_", "FILTER", "BA_DEF_DEF_",
   "EV_DATA_", "ENVVAR_DATA_", "SGTYPE_", "SGTYPE_VAL_", "BA_DEF_SGTYPE_", "BA_SGTYPE_",
   "SIG_TYPE_DEF_", "SIG_TYPE_REF_", "VAL_TABLE_", "SIG_GROUP_", "SIG_VALTYPE_",
-  "SIGTYPE_VALTYPE_", "BO_TX_BU_", "BA_DEF_REL_", "BA_REL_", "BA_DEF_DEF_REL_", 
+  "SIGTYPE_VALTYPE_", "BO_TX_BU_", "BA_DEF_REL_", "BA_REL_", "BA_DEF_DEF_REL_",
   "BU_SG_REL_", "BU_EV_REL_", "BU_BO_REL_"
 };
 
